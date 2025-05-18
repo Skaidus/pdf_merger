6c4ba5934438aaf4cc954eb1cf65a45d29b8b441
mail <you@example.com>
2025-05-18 14:56:36 +0000

    feat: add defaults

--- src/pdf_merger/cli.py
+++ src/pdf_merger/cli.py
@@ -4,8 +4,8 @@ from pathlib import Path
 
 
 @click.command()
-@click.option("--input_dir", type=click.Path(exists=True, file_okay=False, dir_okay=True, path_type=Path), help="The path of the directory that stores the input PDF files")
-@click.option("--output_path", type=click.Path(exists=True, file_okay=True, dir_okay=False, path_type=Path), help="The path of the file that will store the PDF files")
+@click.option("--input_dir", type=click.Path(file_okay=False, dir_okay=True, path_type=Path), default="./data", help="The path of the directory that stores the input PDF files")
+@click.option("--output_path", type=click.Path(file_okay=True, dir_okay=False, path_type=Path), default="./output.pdf", help="The path of the file that will store the PDF files")
 def main(input_dir: Path, output_path: Path):
     # Get all PDF files in the input folder
     pdf_paths = sorted([f for f in input_dir.glob("**/*.pdf")])
